ca83830cb7627218bc1c9ebb7d52feae545d7819
fkatoAppetite <fares@appetitecreative.com>
2022-03-06 15:00:58 +0300

    not 404 handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,3 +19,13 @@ func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+func (h *Handlers) NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	err := h.render(w, r, "404", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering template:", err)
+	}
+
+}
+
